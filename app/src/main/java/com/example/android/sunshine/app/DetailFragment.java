652617b170c8851301340c423c837fee6bb0cb56
João Pedro Nardari dos Santos <joaonardari@terra.com.br>
2015-02-17 16:21:25 -0300

    Detail Screen

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -18,6 +19,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.sunshine.app.data.WeatherContract;
@@ -31,8 +33,9 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
 
     private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
     private String mForecastStr;
-    private TextView forecastDetailTextView;
     private String mSelectedDate;
+
+    private View rootView;
 
     private static final String[] WEATHER_COLUMNS = {
             // In this case the id needs to be fully qualified with a table name, since
@@ -46,7 +49,11 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
             WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
             WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
             WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
-            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+            WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+            WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+            WeatherContract.WeatherEntry.COLUMN_DEGREES,
+            WeatherContract.WeatherEntry.COLUMN_PRESSURE
     };
 
     private static final int WEATHER_LOADER = 0;
@@ -58,6 +65,12 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
     public static final int COL_WEATHER_MAX_TEMP = 3;
     public static final int COL_WEATHER_MIN_TEMP = 4;
     public static final int COL_LOCATION_SETTING = 5;
+
+    // Other info
+    public static final int COL_WEATHER_HUMIDITY = 6;
+    public static final int COL_WEATHER_WIND_SPEED = 7;
+    public static final int COL_WEATHER_DEGREES = 8;
+    public static final int COL_WEATHER_PRESSURE = 9;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -90,9 +103,7 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-        forecastDetailTextView = (TextView) rootView.findViewById(R.id.forecast_detail);
-
+        rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         return rootView;
     }
 
@@ -132,17 +143,52 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         if (cursor.moveToFirst()) {
-            mForecastStr = cursor.getString(COL_WEATHER_DATE) + " - " +
-                    cursor.getString(COL_WEATHER_DESC) + " - " +
-                    cursor.getString(COL_WEATHER_MAX_TEMP) + "/" +
-                    cursor.getString(COL_WEATHER_MIN_TEMP);
 
-            forecastDetailTextView.setText(mForecastStr);
+            // Get Layout References
+            TextView dateTextView      = (TextView) rootView.findViewById(R.id.fragment_detail_date_textview);
+            TextView forecastTextView  = (TextView) rootView.findViewById(R.id.fragment_detail_forecast_textview);
+            TextView highTextView      = (TextView) rootView.findViewById(R.id.fragment_detail_high_textview);
+            TextView lowTextView       = (TextView) rootView.findViewById(R.id.fragment_detail_low_textview);
+
+            ImageView icon             = (ImageView) rootView.findViewById(R.id.fragment_detail_icon);
+
+            TextView humidityTextView  = (TextView) rootView.findViewById(R.id.fragment_detail_humidity_textview);
+            TextView windTextView      = (TextView) rootView.findViewById(R.id.fragment_detail_wind_textview);
+            TextView pressureTextView  = (TextView) rootView.findViewById(R.id.fragment_detail_pressure_textview);
+
+            // Get Data from cursor
+            String weatherDate =  cursor.getString(COL_WEATHER_DATE);
+            String weatherDesc = cursor.getString(COL_WEATHER_DESC);
+            float weatherMaxTemp = cursor.getFloat(COL_WEATHER_MAX_TEMP);
+            float weatherMinTemp = cursor.getFloat(COL_WEATHER_MIN_TEMP);
+            float weatherHumidity = cursor.getFloat(COL_WEATHER_HUMIDITY);
+            float weatherWindSpeed = cursor.getFloat(COL_WEATHER_WIND_SPEED);
+            float weatherDegrees = cursor.getFloat(COL_WEATHER_DEGREES);
+            float weatherPressure = cursor.getFloat(COL_WEATHER_PRESSURE);
+
+            // Share String
+            mForecastStr = weatherDate + " - " +
+                    weatherDesc + " - " +
+                    weatherMaxTemp + "/" +
+                    weatherMinTemp;
+
+            Context context = getActivity();
+
+            // Populate views
+            dateTextView.setText(Util.getFriendlyDayString(context, weatherDate));
+            forecastTextView.setText(weatherDesc);
+
+            boolean isMetric = Util.isMetric(context);
+            highTextView.setText(Util.formatTemperature(context, weatherMaxTemp, isMetric));
+            lowTextView.setText(Util.formatTemperature(context, weatherMinTemp, isMetric));
+
+            humidityTextView.setText(context.getString(R.string.format_humidity, weatherHumidity));
+            windTextView.setText(Util.getFormattedWind(context, weatherWindSpeed, weatherDegrees));
+            pressureTextView.setText(context.getString(R.string.format_pressure, weatherPressure));
         }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        forecastDetailTextView.setText("Reset");
     }
 }
